566654c383278a938ed5c196d43b9f289e40f3b8
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-05-07 15:27:09 +0300

    bugs, flag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       cout: 0,
+      flag: false,
       status: 'Х начинает',
       defaultClass: "gameInfo"
 
@@ -24,37 +25,45 @@ class App extends React.Component {
   }
   isWinner = () => {
     let s = (this.state.cout % 2 === 0) ? 'X' : 'O';
+
     for (let item in this.winnerLine) {
       let line = this.winnerLine[item];
       if (this.state.squares[line[0]] === s
         && this.state.squares[line[1]] === s
         && this.state.squares[line[2]] === s) {
+        this.setState({ flag: true })
         this.setState({ status: 'Победил: ' + s })
         this.setState({ defaultClass: 'gameWin' })
-        setTimeout(this.gameReset, 3000)
+        setTimeout(this.gameReset, 3000);
+
       }
+      else
+        if (this.state.cout === 8 && this.state.flag === false) {
+          this.setState({ status: 'Ничья!' });
+          this.setState({ defaultClass: 'gameOver' })
+          this.setState({ flag: true })
+          setTimeout(this.gameReset, 3000);
+        }
     }
+
   }
+
 
   gameReset = () => {
     this.setState({ squares: Array(9).fill(null) });
     this.setState({ cout: 0 });
+    this.setState({ flag: false });
     this.setState({ status: 'Х начинает' });
     this.setState({ defaultClass: 'gameInfo' })
   }
 
-  gameOver = () => {
-    if (this.state.cout === 8) {
-      this.setState({ status: 'Ничья!' });
-      this.setState({ defaultClass: 'gameOver' })
-      setTimeout(this.gameReset, 3000)
-    }
-  }
 
   clickHandler = (e) => {
+
     let data = e.target.getAttribute('data');
     let currentSqueres = this.state.squares;
-    if (currentSqueres[data] === null) {
+
+    if (currentSqueres[data] === null && this.state.flag === false) {
       if (this.state.cout % 2 === 0) {
         currentSqueres[data] = 'X';
         this.setState({ status: 'O ходит' })
@@ -62,30 +71,28 @@ class App extends React.Component {
         currentSqueres[data] = 'O';
         this.setState({ status: 'X ходит' })
       }
-      // currentSqueres[data] === 'X' ? this.setState({ status: 'O ходит' }) : this.setState({ status: 'X ходит' });
       this.setState({ cout: this.state.cout + 1 });
-      console.log(this.state.cout)
       this.setState({ squares: currentSqueres });
-      console.log(currentSqueres);
     } else {
       this.setState({ status: 'Так нельзя!' })
     }
     this.isWinner();
-    this.gameOver();
   }
 
   render() {
 
-    return (<div className='App App-header'>
-      < div className="Game" > {
-        Object.keys(this.state.squares).map((block, index) => (< div className='block'
-          onClick={this.clickHandler} key={index} data={index}>
-          {this.state.squares[block]}</div>)
-        )}
-      </div>
-      <div className={this.state.defaultClass}>{this.state.status}</div>
-      <button className='gameReset button-primary button-round' onClick={this.gameReset}>Сброс игры</button>
-    </div>)
+    return (
+      <div className='App App-header'>
+        <h1 className='gameName'>Крестики-Нолики</h1>
+        < div className="Game" > {
+          Object.keys(this.state.squares).map((block, index) => (< div className='block'
+            onClick={this.clickHandler} key={index} data={index}>
+            {this.state.squares[block]}</div>)
+          )}
+        </div>
+        <div className={this.state.defaultClass}>{this.state.status}</div>
+        <button className='gameReset button-primary button-round' onClick={this.gameReset}>Сброс игры</button>
+      </div>)
   }
 }
 
