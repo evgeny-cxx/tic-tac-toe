be057c8c165975af8f5c6858b26af27934aaba6b
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-05-08 10:03:09 +0300

    Updates

---
+++ precache-manifest.9c5cd63f9478baea1066fcce1751f135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4c577310c3a78428ee428d77b54c0b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "eea4a17bef3f9d518374",
+    "url": "/tic-tac-toe/static/css/main.e55e17c5.chunk.css"
+  },
+  {
+    "revision": "3c015b61819fa794d60d",
+    "url": "/tic-tac-toe/static/js/2.37eff719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.37eff719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea4a17bef3f9d518374",
+    "url": "/tic-tac-toe/static/js/main.f61b1c3d.chunk.js"
+  },
+  {
+    "revision": "324153dd4300de5bfc1a",
+    "url": "/tic-tac-toe/static/js/runtime-main.9229bfb6.js"
+  }
+]);
