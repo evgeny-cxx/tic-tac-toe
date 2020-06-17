c5e943a906e5a093617f048d507d9f5be2a9dd99
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-06-17 15:45:37 +0300

    mail

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 let title = "Крестики-Нолики";
-let contacts = ['Copyright by cxx_BY', "+375298174777", "evgeny.cxx@gmail.com"];
+let contacts = [
+  "Copyright by cxx_BY",
+  "+375298174777",
+  "evgeny.s.developer@gmail.com",
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title={title}
-      contacts={contacts}
-    />
+    <App title={title} contacts={contacts} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
